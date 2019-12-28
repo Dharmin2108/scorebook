aee973d7ddf2dba39e518bbadb95ea794893a5d7
Dhamin Patel <dharminpatel99@gmail.com>
2019-12-28 13:40:09 +0530

    initial commit

---
+++ src/main/java/com/scorebook/ScorebookApplication.java
@@ -0,0 +1,13 @@
+package com.scorebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scorebook/ScorebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
